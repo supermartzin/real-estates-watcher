75ab721080e0281a77ac923b33f5b57973c9824a
Martin Vrábel <mail@martinvrabel.com>
2021-05-03 17:31:58 +0200

    Initial commit with project template

---
+++ Real Estates Watcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Real_Estates_Watcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
