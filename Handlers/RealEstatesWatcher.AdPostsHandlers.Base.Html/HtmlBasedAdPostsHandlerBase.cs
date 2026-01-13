d687af801476567f173b3cbe475d800195c72163
Martin Vrábel <mail@martinvrabel.com>
2026-01-13 22:49:49 +0100

    Refactor HTML generation into shared base handler.
    Extracted common HTML template and post rendering logic into a new HtmlBasedAdPostsHandlerBase class and project. Updated Email and File handlers to inherit from this base, removing duplicated code. Adjusted project references and solution structure accordingly.

---
+++ Handlers/RealEstatesWatcher.AdPostsHandlers.Base.Html/HtmlBasedAdPostsHandlerBase.cs
@@ -0,0 +1,100 @@
+﻿using RealEstatesWatcher.Models;
+
+using System.Globalization;
+using System.Text;
+using System.Web;
+
+namespace RealEstatesWatcher.AdPostsHandlers.Base.Html;
+
+public abstract class HtmlBasedAdPostsHandlerBase
+{
+    protected virtual string CreateHtmlPostElement(RealEstateAdPost post)
+    {
+        var postHtmlBuilder = new StringBuilder(CommonHtmlTemplateElements.Post)
+            .Replace("{$title}", post.Title)
+            .Replace("{$portal-name}", post.AdsPortalName)
+            .Replace("{$post-link}", post.WebUrl.AbsoluteUri)
+            .Replace("{$address}", post.Address);
+
+        // address links
+        if (!string.IsNullOrEmpty(post.Address))
+        {
+            postHtmlBuilder = postHtmlBuilder.Replace("{$address-links-display}", "inline-block")
+                               .Replace("{$address-encoded}", HttpUtility.UrlEncode(post.Address));
+        }
+        else
+        {
+            postHtmlBuilder = postHtmlBuilder.Replace("{$address-links-display}", "none");
+        }
+
+        // layout
+        postHtmlBuilder = postHtmlBuilder.Replace("{$layout}", post.Layout is not Layout.NotSpecified ? post.Layout.ToDisplayString() : "-");
+
+        // floor area
+        postHtmlBuilder = post.FloorArea is not null and not decimal.Zero ? postHtmlBuilder.Replace("{$floor-area}", post.FloorArea + " m²") : postHtmlBuilder.Replace("{$floor-area}", "-");
+
+        // image
+        if (post.ImageUrl is not null)
+        {
+            postHtmlBuilder = postHtmlBuilder.Replace("{$img-link}", post.ImageUrl.AbsoluteUri)
+                               .Replace("{$img-display}", "block");
+        }
+        else
+        {
+            postHtmlBuilder = postHtmlBuilder.Replace("{$img-link}", string.Empty)
+                               .Replace("{$img-display}", "none");
+        }
+
+        // price
+        if (post.Price is not decimal.Zero)
+        {
+            postHtmlBuilder = postHtmlBuilder.Replace("{$price}", post.Price.ToString("N", new NumberFormatInfo { NumberGroupSeparator = " " }))
+                               .Replace("{$currency}", post.Currency.ToString())
+                               .Replace("{$price-display}", "block")
+                               .Replace("{$price-comment-display}", "none")
+                               .Replace("{$price-comment}", string.Empty);
+        }
+        else
+        {
+            postHtmlBuilder = postHtmlBuilder.Replace("{$price-comment}", post.PriceComment ?? "-")
+                               .Replace("{$price-comment-display}", "block")
+                               .Replace("{$price-display}", "none")
+                               .Replace("{$price}", string.Empty)
+                               .Replace("{$currency}", string.Empty);
+        }
+
+        // additional fees
+        if (post.AdditionalFees is not null and not decimal.Zero)
+        {
+            postHtmlBuilder = postHtmlBuilder.Replace("{$additional-fees}", post.AdditionalFees.Value.ToString("N", new NumberFormatInfo { NumberGroupSeparator = " " }))
+                               .Replace("{$additional-fees-display}", "inline-block");
+        }
+        else
+        {
+            postHtmlBuilder = postHtmlBuilder.Replace("{$additional-fees}", decimal.Zero.ToString("N"))
+                               .Replace("{$additional-fees-display}", "none");
+        }
+
+        // text
+        if (!string.IsNullOrEmpty(post.Text))
+        {
+            postHtmlBuilder = postHtmlBuilder.Replace("{$text}", post.Text)
+                               .Replace("{$text-display}", "table");
+        }
+        else
+        {
+            postHtmlBuilder = postHtmlBuilder.Replace("{$text}", string.Empty)
+                               .Replace("{$text-display}", "none");
+        }
+
+        return postHtmlBuilder.ToString();
+    }
+
+    protected virtual string CreateFullHtmlPage(IEnumerable<RealEstateAdPost> posts, string titleHtmlElement)
+    {
+        return CommonHtmlTemplateElements
+            .FullPage
+            .Replace("<maintitle/>", titleHtmlElement)
+            .Replace("<posts/>", string.Join(Environment.NewLine, posts.Select(CreateHtmlPostElement)));
+    }
+}
--- Handlers/RealEstatesWatcher.AdPostsHandlers.Email/EmailNotifyingAdPostsHandler.cs
+++ Handlers/RealEstatesWatcher.AdPostsHandlers.Email/EmailNotifyingAdPostsHandler.cs
@@ -4,19 +4,16 @@ using Microsoft.Extensions.Logging;
 
 using MimeKit;
 
+using RealEstatesWatcher.AdPostsHandlers.Base.Html;
 using RealEstatesWatcher.AdPostsHandlers.Contracts;
-using RealEstatesWatcher.AdPostsHandlers.Templates;
 using RealEstatesWatcher.Models;
 
-using System.Globalization;
 using System.Net;
-using System.Text;
-using System.Web;
 
 namespace RealEstatesWatcher.AdPostsHandlers.Email;
 
 public class EmailNotifyingAdPostsHandler(EmailNotifyingAdPostsHandlerSettings settings,
-                                          ILogger<EmailNotifyingAdPostsHandler>? logger = null) : IRealEstateAdPostsHandler
+                                          ILogger<EmailNotifyingAdPostsHandler>? logger = null) : HtmlBasedAdPostsHandlerBase, IRealEstateAdPostsHandler
 {
     private readonly EmailNotifyingAdPostsHandlerSettings _settings = settings ?? throw new ArgumentNullException(nameof(settings));
 
@@ -28,7 +25,7 @@ public class EmailNotifyingAdPostsHandler(EmailNotifyingAdPostsHandlerSettings s
 
         logger?.LogDebug("Received new Real Estate Ad Post: {Post}", adPost);
 
-        await SendEmailAsync("🆕 New Real Estate Advert published!", CreateHtmlBody(adPost, CommonHtmlTemplateElements.TitleNewPosts), cancellationToken).ConfigureAwait(false);
+        await SendEmailAsync("🆕 New Real Estate Advert published!", CreateFullHtmlPage([adPost], CommonHtmlTemplateElements.TitleInitialPosts), cancellationToken).ConfigureAwait(false);
     }
 
     public async Task HandleNewRealEstatesAdPostsAsync(IList<RealEstateAdPost> adPosts, CancellationToken cancellationToken = default)
@@ -37,7 +34,7 @@ public class EmailNotifyingAdPostsHandler(EmailNotifyingAdPostsHandlerSettings s
 
         logger?.LogDebug("Received '{PostsCount}' new Real Estate Ad Posts.", adPosts.Count);
 
-        await SendEmailAsync("🆕 New Real Estate Adverts published!", CreateHtmlBody(adPosts, CommonHtmlTemplateElements.TitleNewPosts), cancellationToken).ConfigureAwait(false);
+        await SendEmailAsync("🆕 New Real Estate Adverts published!", CreateFullHtmlPage(adPosts, CommonHtmlTemplateElements.TitleNewPosts), cancellationToken).ConfigureAwait(false);
     }
 
     public async Task HandleInitialRealEstateAdPostsAsync(IList<RealEstateAdPost> adPosts, CancellationToken cancellationToken = default)
@@ -52,7 +49,7 @@ public class EmailNotifyingAdPostsHandler(EmailNotifyingAdPostsHandlerSettings s
 
         logger?.LogDebug("Received initial {PostsCount} Real Estate Ad Posts.", adPosts.Count);
 
-        await SendEmailAsync("🏦 Current Real Estate Adverts offering", CreateHtmlBody(adPosts, CommonHtmlTemplateElements.TitleInitialPosts), cancellationToken).ConfigureAwait(false);
+        await SendEmailAsync("🏦 Current Real Estate Adverts offering", CreateFullHtmlPage(adPosts, CommonHtmlTemplateElements.TitleInitialPosts), cancellationToken).ConfigureAwait(false);
     }
 
     private async Task SendEmailAsync(string subject, string body, CancellationToken cancellationToken = default)
@@ -93,99 +90,5 @@ public class EmailNotifyingAdPostsHandler(EmailNotifyingAdPostsHandlerSettings s
         {
             throw new RealEstateAdPostsHandlerException($"Error during sending email notification: {ex.Message}", ex);
         }
-    }
-
-    private static string CreateHtmlBody(RealEstateAdPost adPost, string titleHtmlElement)
-    {
-        return CommonHtmlTemplateElements.FullPage.Replace("<maintitle/>", titleHtmlElement)
-            .Replace("<posts/>", CreateSingleHtmlPost(adPost));
-    }
-
-    private static string CreateHtmlBody(IEnumerable<RealEstateAdPost> adPosts, string titleHtmlElement)
-    {
-        return CommonHtmlTemplateElements.FullPage.Replace("<maintitle/>", titleHtmlElement)
-            .Replace("<posts/>", string.Join(Environment.NewLine, adPosts.Select(CreateSingleHtmlPost)));
-    }
-
-    private static string CreateSingleHtmlPost(RealEstateAdPost post)
-    {
-        var postHtmlBuilder = new StringBuilder(CommonHtmlTemplateElements.Post)
-            .Replace("{$title}", post.Title)
-            .Replace("{$portal-name}", post.AdsPortalName)
-            .Replace("{$post-link}", post.WebUrl.AbsoluteUri)
-            .Replace("{$address}", post.Address);
-
-        // address links
-        if (!string.IsNullOrEmpty(post.Address))
-        {
-            postHtmlBuilder = postHtmlBuilder.Replace("{$address-links-display}", "inline-block")
-                               .Replace("{$address-encoded}", HttpUtility.UrlEncode(post.Address));
-        }
-        else
-        {
-            postHtmlBuilder = postHtmlBuilder.Replace("{$address-links-display}", "none");
-        }
-
-        // layout
-        postHtmlBuilder = postHtmlBuilder.Replace("{$layout}", post.Layout is not Layout.NotSpecified ? post.Layout.ToDisplayString() : "-");
-
-        // floor area
-        postHtmlBuilder = post.FloorArea is not null and not decimal.Zero ? postHtmlBuilder.Replace("{$floor-area}", post.FloorArea + " m²") : postHtmlBuilder.Replace("{$floor-area}", "-");
-
-        // image
-        if (post.ImageUrl is not null)
-        {
-            postHtmlBuilder = postHtmlBuilder.Replace("{$img-link}", post.ImageUrl.AbsoluteUri)
-                               .Replace("{$img-display}", "block");
-        }
-        else
-        {
-            postHtmlBuilder = postHtmlBuilder.Replace("{$img-link}", string.Empty)
-                               .Replace("{$img-display}", "none");
-        }
-
-        // price
-        if (post.Price is not decimal.Zero)
-        {
-            postHtmlBuilder = postHtmlBuilder.Replace("{$price}", post.Price.ToString("N", new NumberFormatInfo { NumberGroupSeparator = " " }))
-                               .Replace("{$currency}", post.Currency.ToString())
-                               .Replace("{$price-display}", "block")
-                               .Replace("{$price-comment-display}", "none")
-                               .Replace("{$price-comment}", string.Empty);
-        }
-        else
-        {
-            postHtmlBuilder = postHtmlBuilder.Replace("{$price-comment}", post.PriceComment ?? "-")
-                               .Replace("{$price-comment-display}", "block")
-                               .Replace("{$price-display}", "none")
-                               .Replace("{$price}", string.Empty)
-                               .Replace("{$currency}", string.Empty);
-        }
-
-        // additional fees
-        if (post.AdditionalFees is not null and not decimal.Zero)
-        {
-            postHtmlBuilder = postHtmlBuilder.Replace("{$additional-fees}", post.AdditionalFees.Value.ToString("N", new NumberFormatInfo { NumberGroupSeparator = " " }))
-                               .Replace("{$additional-fees-display}", "inline-block");
-        }
-        else
-        {
-            postHtmlBuilder = postHtmlBuilder.Replace("{$additional-fees}", decimal.Zero.ToString("N"))
-                               .Replace("{$additional-fees-display}", "none");
-        }
-
-        // text
-        if (!string.IsNullOrEmpty(post.Text))
-        {
-            postHtmlBuilder = postHtmlBuilder.Replace("{$text}", post.Text)
-                               .Replace("{$text-display}", "table");
-        }
-        else
-        {
-            postHtmlBuilder = postHtmlBuilder.Replace("{$text}", string.Empty)
-                               .Replace("{$text-display}", "none");
-        }
-
-        return postHtmlBuilder.ToString();
     }
 }
