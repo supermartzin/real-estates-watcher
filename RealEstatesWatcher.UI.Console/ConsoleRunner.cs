5fda87b896bad8666dec0228d57350e70a816cf6
Martin Vrábel <mail@martinvrabel.com>
2026-02-10 17:15:09 +0100

    Improved value check.

--- RealEstatesWatcher.UI.Console/ConsoleRunner.cs
+++ RealEstatesWatcher.UI.Console/ConsoleRunner.cs
@@ -147,15 +147,9 @@ public class ConsoleRunner
 
 #if DEBUG
         if (string.IsNullOrEmpty(settings.ProjectId))
-        {
-            Logger.LogWarning("Google Cloud Logging is enabled and the app is running in DEBUG mode but Project ID is not set, skipping further configuration.");
-            return;
-        }
+            Logger.LogWarning("Google Cloud Logging is enabled and the app is running in DEBUG mode but Project ID is not set, logging might not work as expected.");
         if (string.IsNullOrEmpty(settings.ServiceName))
-        {
-            Logger.LogWarning("Google Cloud Logging is enabled and the app is running in DEBUG mode but Service Name is not set, skipping further configuration.");
-            return;
-        }
+            Logger.LogWarning("Google Cloud Logging is enabled and the app is running in DEBUG mode but Service Name is not set, logging might not work as expected.");
 
         loggingBuilder.AddGoogle(new LoggingServiceOptions
         {
